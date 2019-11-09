9b086237aa794e61df7624390af2c2f234e5772b
Samuel Martin <Samuel Martin>
2019-11-09 16:49:48 +0000

    added code to chose number of symbolic elements in matrix

--- inverse_matrix.py
+++ inverse_matrix.py
@@ -17,13 +17,21 @@ out for a question sheet.
 import os
 import MatrixQuestioner as mq
 
-rows = int(input('\nHow many rows in matrices?\n \
-    enter 3 for a 3 x 3 matrix: '))
+#  Get Matix size
+print('\n\nWhat size of matrix is required?')
+rows = int(input('\n enter 3 for a 3 x 3 matrix: '))
+#  Get number of questions required
 quantity = int(input('How many matrices do you want? '))
+#  Choose whether to include sybolic variables.
+print('\nThe matrix can contain non-numberical symblols')
+print('Press ENTER or 0 for a numerical matrix')
+symbs = int(input('How many elements with symblols? '))
+if not symbs:
+    symbs = 0
 
 questions = []
 for i in range(quantity):
-    matrix = mq.MatrixQuestioner(rows)
+    matrix = mq.MatrixQuestioner(rows, symbols=symbs)
     A, inv_A = matrix.inverse()
     question = 'A = {}\t\tinv(A) = {}\n\n'.format(A, inv_A)
     questions.append(question)
